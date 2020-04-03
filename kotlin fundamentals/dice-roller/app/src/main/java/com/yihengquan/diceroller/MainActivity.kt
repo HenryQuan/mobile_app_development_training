5cf16a7a31055506c43d8c03cdf74ede55589f0e
Yiheng (NUC) <development.henryquan@gmail.com>
2020-04-03 17:50:13 +1100

    a working dice roller
    this is simple but surprisingly fun.
    they are doing a great job so far.

--- kotlin fundamentals/dice-roller/app/src/main/java/com/yihengquan/diceroller/MainActivity.kt
+++ kotlin fundamentals/dice-roller/app/src/main/java/com/yihengquan/diceroller/MainActivity.kt
@@ -2,11 +2,28 @@ package com.yihengquan.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll"
+        rollButton.setOnClickListener {
+            rollDice()
+            Toast.makeText(this, "Dice is rolled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun rollDice() {
+        val textView: TextView = findViewById(R.id.result_text)
+        val dice = Random().nextInt(6) + 1
+        textView.text = dice.toString()
     }
 }
