70b0375aab377ffaae3986e1d343233e2127ec7a
Yiheng <development.henryquan@gmail.com>
2020-04-05 13:37:05 +1000

    Section 2

---
+++ iOS/bootcamp/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yiheng Quan on 5/4/20.
+//  Copyright © 2020 Yiheng Quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
