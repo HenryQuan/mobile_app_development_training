27cc8393c6d4c2e075d4885e8ebcf5ab579a32ce
Yiheng <development.henryquan@gmail.com>
2020-05-11 11:10:41 +1000

    I Am Poor

---
+++ iOS/bootcamp/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Yiheng Quan on 11/5/20.
+//  Copyright © 2020 Yiheng Quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
