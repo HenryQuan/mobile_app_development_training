25e221c5126fb70d5160ec452d76c308d049fa99
Yiheng <development.henryquan@gmail.com>
2020-05-13 10:52:24 +1000

    random images

--- iOS/bootcamp/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ iOS/bootcamp/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    let images = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollBtnTap(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageView1.image = images[Int.random(in: 0...5)]
+        diceImageView2.image = images[Int.random(in: 0...5)]
     }
 }
 
