f7310eea3adb7911d409645a8f3db448cc0f7f4a
Yiheng <development.henryquan@gmail.com>
2020-05-15 16:42:43 +1000

    working app with the help of `tag`

--- iOS/bootcamp/Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ iOS/bootcamp/Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -11,7 +11,8 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
+    let soundList = ["C", "D", "E", "F", "G", "A", "B"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +20,13 @@ class ViewController: UIViewController {
 
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(name: "C")
+        playSound(name: soundList[sender.tag])
     }
     
     func playSound(name: String) {
-        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else { return }
-        
-        do {
-            let audio = AVAudioSession.sharedInstance()
-            try audio.setCategory(.playback, mode: .default)
-            try audio.setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            player?.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
     
 }
