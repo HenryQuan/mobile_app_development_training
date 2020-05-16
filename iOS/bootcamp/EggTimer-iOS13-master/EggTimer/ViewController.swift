ee48782311c195c52cff10071c61b864c9d9e11e
Yiheng <development.henryquan@gmail.com>
2020-05-16 14:06:02 +1000

    cancel old timer and update timer

--- iOS/bootcamp/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ iOS/bootcamp/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes: [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    private let eggTimes: [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    private var timer = Timer()
 //    let softTime = 5
 //    let mediumTime = 7
 //    let hardTime = 12
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle
-        var time = eggTimes[hardness!]! * 60
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (t) in
+        var time = eggTimes[hardness!]!
+        
+        // Cancel it first
+        timer.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (t) in
             if time == 0 {
                 t.invalidate()
                 print("Completed!")
+                self.titleLabel.text = "DONE"
             } else {
                 print("\(time)s remaining")
                 time -= 1
             }
-        }.fire()
+        }
+        timer.fire()
     }
     
 }
