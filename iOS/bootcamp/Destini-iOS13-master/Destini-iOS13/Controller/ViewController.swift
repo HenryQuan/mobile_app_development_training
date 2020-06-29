e0a656ffa3614f64f94636cb384cb72c35dc1b4c
Yiheng <development.henryquan@gmail.com>
2020-06-29 13:07:34 +1000

    added a struct and updated label text

--- iOS/bootcamp/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
+++ iOS/bootcamp/Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story = Story(title: "This is the title", choice1: "Yes", choice2: "No")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
 
 }
 
