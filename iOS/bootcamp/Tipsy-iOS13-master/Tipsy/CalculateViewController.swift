941971c4cc74cc07646dfdd8905effa630125a36
Yiheng <development.henryquan@gmail.com>
2020-07-01 13:40:58 +1000

    setup and rename

---
+++ iOS/bootcamp/Tipsy-iOS13-master/Tipsy/CalculateViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func onTipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func onStepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func onCalculateButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+}
+
